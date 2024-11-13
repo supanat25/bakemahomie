c9025e12f0e6c0a6f4c878adff52527aa6ee89d3
supanat25 <supanat1012@gmail.com>
2024-11-13 15:22:20 +0700

    spilt data file

---
+++ script/imageData.js
@@ -0,0 +1,53 @@
+export  const cakeImages = [
+    { path: "img/cake/brownieChoc.jpg", name: "เค้กช็อกโกแลต" },
+    { path: "img/cake/cakeChoc.jpg", name: "เค้กวานิลลา" },
+    { path: "img/cake/cakeChoc2.jpg", name: "เค้กสตรอว์เบอร์รี" },
+    { path: "img/cake/lemonCake.jpg", name: "เค้กช็อกโกแลต" },
+    { path: "img/cake/redvel.jpg", name: "เค้กวานิลลา" },
+    { path: "img/cake/redvel2.jpg", name: "เค้กสตรอว์เบอร์รี" },
+    { path: "img/cake/banana.jpg", name: "เค้กสตรอว์เบอร์รี" },
+    { path: "img/cake/banof.jpg", name: "เค้กช็อกโกแลต" },
+    { path: "img/cake/lemonCake2.jpg", name: "เค้กวานิลลา" },
+    { path: "img/cake/redvel3.jpg", name: "เค้กสตรอว์เบอร์รี" },
+    { path: "img/cake/orangeCake.jpg", name: "เค้กสตรอว์เบอร์รี" },
+];
+
+export const cheeseImages = [
+    { path: "img/cheese/choc.jpg", name: "ขนมปังฝรั่งเศส" },
+    { path: "img/cheese/blue.jpg", name: "ขนมปังโฮลวีต" },
+    { path: "img/cheese/cheese.jpg", name: "ขนมปังเนย" },
+    { path: "img/cheese/lemon.jpg", name: "ขนมปังฝรั่งเศส" },
+    { path: "img/cheese/orange.jpg", name: "ขนมปังโฮลวีต" },
+    { path: "img/cheese/straw.jpg", name: "ขนมปังเนย" },
+    { path: "img/cheese/cheeseBlue.jpg", name: "ขนมปังฝรั่งเศส" },
+    { path: "img/cheese/lemon2.jpg", name: "ขนมปังโฮลวีต" },
+    { path: "img/cheese/cheeseBlue2.jpg", name: "ขนมปังเนย" }
+];
+
+export const tardImages = [
+    { path: "img/tard/lemonTard.jpg", name: "ขนมปังฝรั่งเศส" },
+    { path: "img/tard/tardGreen.jpg", name: "ขนมปังโฮลวีต" },
+    { path: "img/tard/tardGreen2.jpg", name: "ขนมปังเนย" },
+
+];
+
+export const pondImages = [
+    { path: "img/pond/p1.jpg", name: "ขนมปังฝรั่งเศส" },
+    { path: "img/pond/p2.jpg", name: "ขนมปังฝรั่งเศส" },
+    { path: "img/pond/p3.jpg", name: "ขนมปังฝรั่งเศส" },
+    { path: "img/pond/p4.jpg", name: "ขนมปังฝรั่งเศส" },
+    { path: "img/pond/p5.jpg", name: "ขนมปังฝรั่งเศส" },
+    { path: "img/pond/p6.jpg", name: "ขนมปังฝรั่งเศส" },
+    { path: "img/pond/p7.jpg", name: "ขนมปังฝรั่งเศส" },
+];
+
+
+
+export const dry = [
+ { path: "img/dry/conflask.jpg", name: "ขนมปังฝรั่งเศส" },
+ { path: "img/dry/softCook.jpg", name: "ขนมปังโฮลวีต" },
+ { path: "img/dry/brownie.jpg", name: "ขนมปังเนย" },
+ { path: "img/dry/softCook2.jpg", name: "ขนมปังเนย" },
+];
+
+export const allImages = [...cakeImages, ...cheeseImages, ...dry , ...tardImages, ...pondImages];
